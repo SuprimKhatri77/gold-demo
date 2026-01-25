4577639eab84bc734c7383b66e4bdb3c4861dc96
Suprim Khatri <Supremekhatri62@gmail.com>
2026-01-25 19:16:01 +0545

    edited the company detail file

--- utils/info/details.ts
+++ utils/info/details.ts
@@ -2,6 +2,6 @@ export const companyDetails = {
   phoneNumber: "+971 4 225 5442",
   email: "info@srgold.com",
   longAddress:
-    "Near Bait Al Banat Women&apos;s Museum, Al Sabkha, Gold Souq, Near - Al Khor Street - Deira - Dubai 14925",
+    "Near Bait Al Banat Women's Museum, Al Sabkha, Gold Souq, Near - Al Khor Street - Deira - Dubai 14925",
   shortAddress: "Gold Souq, Deira - Dubai, UAE",
 };
